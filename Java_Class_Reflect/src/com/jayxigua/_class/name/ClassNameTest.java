030e7cf198634492223491478c1cac51e8ec597e
jianbin.zou <jianbin.zou@dianping.com>
2014-09-10 18:01:44 +0800

    class reflect

---
+++ Java_Class_Reflect/src/com/jayxigua/_class/name/ClassNameTest.java
@@ -0,0 +1,15 @@
+package com.jayxigua._class.name;
+
+public class ClassNameTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassNameTest t=new ClassNameTest();
+		System.out.println(t.getClass());
+		System.out.println(t.getClass().getName());
+	}
+
+}
